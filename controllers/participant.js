6bc2e055d44c9dc7d8c00c573511af693546a1ad
YohannCstd <yohann.costard@ynov.com>
2023-06-01 22:51:46 +0200

    Ajout dun participant

--- controllers/participant.js
+++ controllers/participant.js
@@ -1,5 +1,20 @@
 const participantModel = require("../models/participant.js");
 
+esports.addParticipant = async (req, res) => {
+  const { userId , petId, postId } = req.body;
+
+  if(!userId || !petId || !postId) {
+    return res.status(400).json({ error: "Missing parameters" });
+  }
+
+  try {
+    await participantModel.create({ postId, userId, petId, status: "pending", statusMessage: "NA" });
+    return res.status(201).json({ message: "Participant added successfully!" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 exports.deleteById = async (req, res) => {
   const { id } = req.params;
