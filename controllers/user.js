8e33f70e4911d211bda078c7343d982f3c86791b
YohannCstd <yohann.costard@ynov.com>
2023-06-01 21:45:41 +0200

    Modification user

--- controllers/user.js
+++ controllers/user.js
@@ -3,22 +3,6 @@ const userModel = require("../models/user");
 const generateToken = require("../middleware/auth");
 const mailService = require("../services/mail");
 const shortid = require('shortid');
-const multer = require('multer');
-
-// Définir le dossier de destination pour les avatars
-const avatarStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(process.cwd(),'../stockage'));
-  },
-  filename: function (req, file, cb) {
-    // Générer un nom de fichier unique en ajoutant un timestamp au nom d'origine
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
-  }
-});
-
-// Configurer le middleware multer avec la configuration de stockage
-const uploadAvatar = multer({ storage: avatarStorage }).single('avatar');
 
 exports.register = async (req, res) => {
   let { name, firstname, birthdate, password, email, adress, latitude, longitude, avatar, description } = req.body;
@@ -45,56 +29,39 @@ exports.register = async (req, res) => {
     return res.status(400).json({ error: "User already exists" });
   }
 
-  // Appeler le middleware multer pour gérer le téléchargement de l'avatar
-  uploadAvatar(req, res, async (err) => {
-    if (err instanceof multer.MulterError) {
-      // Une erreur de téléchargement s'est produite
-      return res.status(500).json({ error: 'Avatar upload error' });
-    } else if (err) {
-      // Une autre erreur s'est produite
-      return res.status(500).json({ error: 'Internal server error' });
+  // Crypter le mot de passe
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error" });
     }
 
-    // Le téléchargement de l'avatar s'est déroulé avec succès
-
-    // Récupérer le chemin du fichier téléchargé
-    const avatarPath = req.file.path;
-
-    // Crypter le mot de passe
-    bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ error: "Internal server error" });
       }
 
-      bcrypt.hash(password, salt, async (err, hash) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: "Internal server error" });
-        }
+      password = hash;
 
-        password = hash;
-
-        // Enregistrer l'utilisateur dans la base de données avec le chemin de l'avatar
-        await userModel.create({
-          name,
-          firstname,
-          birthdate,
-          password,
-          email,
-          adress,
-          latitude,
-          longitude,
-          avatar: avatarPath,
-          description
-        });
-
-        return res.status(200).json({ message: "User registered successfully" });
+      // Enregistrer l'utilisateur dans la base de données
+      await userModel.create({
+        name,
+        firstname,
+        birthdate,
+        password,
+        email,
+        adress,
+        latitude,
+        longitude,
+        avatar,
+        description
       });
+
+      return res.status(200).json({ message: "User registered successfully" });
     });
   });
 };
-
 
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
